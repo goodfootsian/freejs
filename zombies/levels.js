d40fe1a181bf159bd82892e2b703b4036e3518a0
Xian Saleh <sianturi.saleh@gmail.com>
2025-06-27 15:11:09 +0200

    Projektdateien hinzufügen.

---
+++ zombies/levels.js
@@ -0,0 +1,215 @@
+var images = [];
+images['0'] = 'suna.png'
+images['1'] = 'talara.png'
+images['2'] = 'cov-19.png'
+images['3'] = 'Gibson.png'
+images['4'] = 'Squier.png'
+images['5'] = 'campfire.png'
+images['6'] = 'code.png'
+images['7'] = 'wm.png'
+images['8'] = 'pizza.png'
+images['9'] = 'tadjine.png'
+images['a'] = 'beeftarar.png'
+images['b'] = 'Maya Bild Gesichter.jpg'
+images['c'] = 'Bild Maya Ornament.jpg'
+
+class lab_class {
+    constructor() {
+        this.title = "to be defined";
+        this.matrix = [];
+    }
+}
+
+
+var lab = [];
+//
+var thislab = new lab_class;
+lab.push(thislab);
+thislab.title = "The Gallery"
+thislab.matrix.push("                                                +-W-+-W-+                                                                ");
+thislab.matrix.push("                                                | Z1  A |                                                                ");
+thislab.matrix.push("                                                +       +                                                                ");
+thislab.matrix.push("                                                |       |                                                                ");
+thislab.matrix.push("                                                +       +                                                                ");
+thislab.matrix.push("                                                |       |                                                                ");
+thislab.matrix.push("+-w-+---+-w-+---+-w-+-pb+-w-+---+---+---+---+---+-D-+-W-+---+---+---+---+---+---+---+---+---+---+---+---+---+---+-----w-+");
+thislab.matrix.push("+                         A | H |                                       | H |                                   |       w");
+thislab.matrix.push("+                           +   +                                       +   +                                   +       +");
+thislab.matrix.push("+ E   M   L                 D Z1D                                       D Z2D Z6                                D     Z2w");
+thislab.matrix.push("+                           +   +                                       +   +                                   +       +");
+thislab.matrix.push("+ A                         | H |                                       | H |                                   | T     w");
+thislab.matrix.push("+-w-+---+-w-+---+-w-+-pc+-w-+---+---+---+---+---+-D-+-W-+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+-w-+");
+thislab.matrix.push("                                                | Z1  A |                                                                ");
+thislab.matrix.push("                                                +       +                                                                ");
+thislab.matrix.push("                                                |       |                                                                ");
+thislab.matrix.push("                                                +       +                                                                ");
+thislab.matrix.push("                                                |       |                                                                ");
+thislab.matrix.push("                                                +-W-+-W-+                                                                ");
+//
+var thislab = new lab_class;
+lab.push(thislab);
+thislab.title = "The Stage"
+thislab.matrix.push("+---+---+---+---+---+---+---+---+---+---+");
+thislab.matrix.push("+                                 H | T +");
+thislab.matrix.push("+   +---+---+-D-+---+---+-D-+---+---+   +");
+thislab.matrix.push("+   +                             Z2|   +");
+thislab.matrix.push("+   +                               +   +");
+thislab.matrix.push("+   D                                   +");
+thislab.matrix.push("+   +                                   +");
+thislab.matrix.push("+   |                                   p6");
+thislab.matrix.push("+   +                                   +");
+thislab.matrix.push("+   D                                   p5");
+thislab.matrix.push("+   +                                   +");
+thislab.matrix.push("+   |                                   p7");
+thislab.matrix.push("+   +                                   +");
+thislab.matrix.push("+   D                                   +");
+thislab.matrix.push("+   +                               +   +");
+thislab.matrix.push("+   +                               |   +");
+thislab.matrix.push("+   +---+---+ D +---+---+ D +---+---+   +");
+thislab.matrix.push("+ E   L                           M | A +");
+thislab.matrix.push("+---+---+---+---+---+---+---+---+---+---+");
+//
+var thislab = new lab_class;
+lab.push(thislab);
+thislab.title = "Cubus"
+thislab.matrix.push("+---+---+---+---+---+---+---+---+---+---+---+---+---+");
+thislab.matrix.push("+ E     | L       M                               H  ");
+thislab.matrix.push("+       +       +-w-+-w-+  -+-w-+                   +");
+thislab.matrix.push("+       |       w     Z3w   w Z2w                    ");
+thislab.matrix.push("+       +       +       +   +   +                   +");
+thislab.matrix.push("+       |       w       w   w   w                    ");
+thislab.matrix.push("+       +       +-w-+-w-+  -+-w-+                   +");
+thislab.matrix.push("+                                                 T +");
+thislab.matrix.push("+---------------------------------------------------+");
+//
+var thislab = new lab_class;
+lab.push(thislab);
+thislab.title = "Ruban Virus Lab"
+thislab.matrix.push("+---+---+---+---+---+---+---+---+---+");
+thislab.matrix.push("+         Z5w         A         w   +");
+thislab.matrix.push("+   +-w-+-w-+   +-w-+-w-+-w-+   +   +");
+thislab.matrix.push("+   w z2      z2w H   T   H w z2w   +");
+thislab.matrix.push("+   +   +-w-+-w-+           +-w-+   +");
+thislab.matrix.push("+   w   w                     A     +");
+thislab.matrix.push("+   +   +-w-+-w-+   +-w-+   +-w-+   +");
+thislab.matrix.push("+   w z2      z2w   w z2w   w z2w   +");
+thislab.matrix.push("+   +   +-w-+-w-+   +-w-+   +-w-+   +");
+thislab.matrix.push("+   w   w                           +");
+thislab.matrix.push("+   +   +-w-+-w-+   +-w-+   +-w-+   +");
+thislab.matrix.push("+   w z2      z2w   w z2w   w z2w   +");
+thislab.matrix.push("+   +   +-w-+-w-+   +-w-+   +-w-+   +");
+thislab.matrix.push("+   w   w                           +");
+thislab.matrix.push("+   +   +-w-+-w-+   +-w-+   +-w-+   +");
+thislab.matrix.push("+   w z2      z2w   w z2w   w z2w   +");
+thislab.matrix.push("+   +-w-+-w-+-w-+   +-w-+   +-w-+   +");
+thislab.matrix.push("p2                                  P2");
+thislab.matrix.push("+                                   +");
+thislab.matrix.push("p2                                  P2");
+thislab.matrix.push("+                                   +");
+thislab.matrix.push("p2                                  P2");
+thislab.matrix.push("+                                   +");
+thislab.matrix.push("+                                   +");
+thislab.matrix.push("+---+---+---+-D-+-D-+---+---+---+---+");
+thislab.matrix.push("+         A | L   Z3              A +");
+thislab.matrix.push("+---+---+   +---+---+---+   +---+---+");
+thislab.matrix.push("+ A             D                 H +");
+thislab.matrix.push("+---+---+-d-+-d-+---+---+---+---+---+");
+thislab.matrix.push("  E       M                       Z  ");
+thislab.matrix.push("                                     ");
+//
+var thislab = new lab_class;
+lab.push(thislab);
+thislab.title = "The House of Z"
+thislab.matrix.push("                        +-W-+-W-+                            ");
+thislab.matrix.push("                        W H   H W                         T  ");
+thislab.matrix.push("+---+-P8+-Pa+-P9+---+---+       +---+---+---+---+---+        ");
+thislab.matrix.push("| H   H   H   H   H |     H   H                     D        ");
+thislab.matrix.push("+                   +                               +        ");
+thislab.matrix.push("W                   D     H   H                   A W        ");
+thislab.matrix.push("+                   +                               +        ");
+thislab.matrix.push("| H   H   H   H   H |                             A W        ");
+thislab.matrix.push("+-------------------+                               +        ");
+thislab.matrix.push("+         A   A   A |                             A W        ");
+thislab.matrix.push("+                   +   +---+---+---+               +        ");
+thislab.matrix.push("W                   |   |         H +             A W        ");
+thislab.matrix.push("+                   +   +           +---+---+---+---+-D-+    ");
+thislab.matrix.push("W                   D   D H         +     A   A   A     |    ");
+thislab.matrix.push("+                   +   +---+---+---+                   +    ");
+thislab.matrix.push("W                   +           D                       W    ");
+thislab.matrix.push("+---+-P4+-P0+---+---+   +---+-D-+---+                   |    ");
+thislab.matrix.push("W       P3A   A   Z |   +         H |                   W    ");
+thislab.matrix.push("+       +           +   +           +                   |    ");
+thislab.matrix.push("W                   D   +         H |                   W    ");
+thislab.matrix.push("+                   +   +           +                   |    ");
+thislab.matrix.push("W                   |   + A   H   H |                   |    ");
+thislab.matrix.push("+---+---+---+---+---+-D-+-----------+---+---+---+---+-P1----+");
+thislab.matrix.push("W   |                   W                     Z9            w");
+thislab.matrix.push("+   +---+---+---+---+   +                                   +");
+thislab.matrix.push("W E   M   L           Z9D                                   w");
+thislab.matrix.push("+---+---+---+---+---+---+-----------------------------------+");
+//
+/*
+var thislab = new lab_class;
+lab.push(thislab);
+thislab.matrix.push("+---+---+-------+---+---+---+---+---+");
+thislab.matrix.push("+ E   M   A   L + Z1              T +");
+thislab.matrix.push("+---+---+       +---+---+   +---+---+");
+thislab.matrix.push("+ A             D                 H +");
+thislab.matrix.push("+---+---+---+---+---+---+---+---+---+");
+//
+var thislab = new lab_class;
+lab.push(thislab);
+thislab.matrix.push("+---+---+---+---+---+---+---+---+---+---+");
+thislab.matrix.push("+                                       +");
+thislab.matrix.push("+                       +---+---+---+   +");
+thislab.matrix.push("+                       D     Z1  T +   +");
+thislab.matrix.push("+                       +---+   +---+   +");
+thislab.matrix.push("+         L                 +   + A     +");
+thislab.matrix.push("+---+---+                   +   +---+   +");
+thislab.matrix.push("+     M                     +     H +   +");
+thislab.matrix.push("+                           +---+---+   +");
+thislab.matrix.push("+ E                                     +");
+thislab.matrix.push("+---+---+---+---+---+---+---+---+---+---+");
+*/
+//
+var thislab = new lab_class;
+lab.push(thislab);
+thislab.title = "The Small Labyrinth"
+thislab.matrix.push("+---+---+---+---+---+---+---+---+---+---+");
+thislab.matrix.push("+                   +                 L +");
+thislab.matrix.push("+                   +   +---+   +---+   +");
+thislab.matrix.push("+                   +   +     Z2    +   +");
+thislab.matrix.push("+                   +   +---+   +---+   +");
+thislab.matrix.push("+ A                 +     H +   +       +");
+thislab.matrix.push("+---+---+       +---+       +   +---+   +");
+thislab.matrix.push("+ E   M +     L +           +     T +   +");
+thislab.matrix.push("+       +       +           +---+---+   +");
+thislab.matrix.push("+       D                               +");
+thislab.matrix.push("+---+---+---+---+---+---+---+---+---+---+");
+//
+var thislab = new lab_class;
+lab.push(thislab);
+thislab.title = "The Big Labyrinth Bag Ma'm"
+thislab.matrix.push("+---+---+---+---+---+---+---+---+---+---+");
+thislab.matrix.push("+                   +                 L +");
+thislab.matrix.push("+   +---+   +---+   +   +---+   +---+   +");
+thislab.matrix.push("+   +   +   +   +   +   +   +   +   +   +");
+thislab.matrix.push("+   +---+   +---+   +   +---+   +---+   +");
+thislab.matrix.push("+                                       +");
+thislab.matrix.push("+   +---+---+   +---+---+   +---+---+   +");
+thislab.matrix.push("+   +       +   +       +   +       +   +");
+thislab.matrix.push("+   +---+---+   +---+---+   +---+---+   +");
+thislab.matrix.push("+                                       +");
+thislab.matrix.push("+           +   +       +   +           +");
+thislab.matrix.push("+           +   +       +   +           +");
+thislab.matrix.push("+           +   +       +   +           +");
+thislab.matrix.push("+ E   M     D   + Z6  T +   +           +");
+thislab.matrix.push("+---+---+   +   +-------+   +   +---+---+");
+thislab.matrix.push("        +   +               +   +       +");
+thislab.matrix.push("+---+---+   +   +-------+   +   +---+   +");
+thislab.matrix.push("+           +   +       +   +           +");
+thislab.matrix.push("+   +---+---+   +       +   +---+---+   +");
+thislab.matrix.push("+   + H                           H +   +");
+thislab.matrix.push("+   +---+---+---+---+---+---+---+---+   +");
+thislab.matrix.push("+                                       +");
+thislab.matrix.push("+---+---+---+---+---+---+---+---+---+---+");
